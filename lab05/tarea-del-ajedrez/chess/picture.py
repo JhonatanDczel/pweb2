15190188b84ac5da05b7ccf5b69af3b943519af2
JhonatanDczel <jariasq@unsa.edu.pe>
2024-05-31 18:52:37 -0500

    Completando todos los metodos

--- lab05/tarea-del-ajedrez/chess/picture.py
+++ lab05/tarea-del-ajedrez/chess/picture.py
@@ -18,47 +18,90 @@ class Picture:
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
     vertical = []
-    for value in self.img:
-      vertical.append(value[::-1])
-    return vertical
+    for fila in self.img:
+      espejo = ""
+      for letra in fila:
+        espejo = letra + espejo
+      vertical.append(espejo)
+    return Picture(vertical)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    return Picture(None)
+    horizontal = []
+
+    for i in range(len(self.img)):
+      l = len(self.img) - i - 1
+      horizontal.append(self.img[l])
+
+    return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
     new_img = []
     for string in self.img:
       new_string = [self._invColor(letter) for letter in string]
+      new_img.append(new_string)
+    print(new_img)
     return Picture(new_img)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
+    new_img = []
+    l = max(len(self.img), len(p.img))
+
+    for i in range(l):
+      izquierda = self.img[i]
+      derecha = p.img[i]
+      if izquierda is None:
+        izquierda = [" " for i in range(len(self.img[0]))]
+      if derecha is None:
+        derecha = [" " for i in range(len(p.img[0]))]
+      row = izquierda + derecha
+      new_img.append(row)
+
+    return Picture(new_img)
+
+  """ Devuelve una nueva figura poniendo la figura actual
+      sobre la figura p """
 
   def up(self, p):
-    return Picture(None)
+    new_img = self.img + p.img
+    return Picture(new_img)
 
   def under(self, p):
-    """ Devuelve una nueva figura poniendo la figura p sobre la
-        figura actual """
-    return Picture(None)
+    """ Devuelve una nueva figura poniendo la figura actual debajo
+        de la figura p """
+    new_img = p.img + self.img
+    return Picture(new_img)
 
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    new_img = []
+    for row in self.img:
+      new_row = row * n
+      new_img.append(new_row)
+
+    return Picture(new_img)
 
   def verticalRepeat(self, n):
-    return Picture(None)
+    new_img = self.img * n
+    return Picture(new_img)
 
   #Extra: Sólo para realmente viciosos
-  def rotate(self):
+  def rotate_horario(self):
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
     o antihorario"""
-    return Picture(None)
+    transpuesta = list(zip(*self.img))
+
+    rotada = [list(fila)[::-1] for fila in transpuesta]
+
+    return Picture(rotada)
+  
+  def rotate_antihorario(self):
+    return self.rotate_horario().rotate_horario().rotate_horario()
+    
 
   @staticmethod
   def rock():
--- lab05/tarea-del-ajedrez/test.py
+++ lab05/tarea-del-ajedrez/test.py
@@ -1,9 +1,82 @@
 from chess.picture import Picture
 from chess.interpreter import draw
 
+test = None
+torre = Picture.rock()
+caballo = Picture.knight()
+rey = Picture.king()
+dama = Picture.queen()
+peon = Picture.pawn()
+
 
 def test_invColor():
-  torre = Picture.rock()
-  draw(torre)
+  global test
+  test = torre.negative()
 
-test_invColor()
+
+def test_espejos():
+  global test
+  test = torre.negative()
+  torreHorizontal = torre.horizontalMirror()
+  caballoVertical = caballo.verticalMirror()
+  test = torreHorizontal
+
+
+def test_join():
+  global test
+  test = torre.negative()
+  pareja = rey.join(dama)
+  test = pareja
+
+
+def test_up():
+  global test
+  test = torre.negative()
+  up = rey.up(peon)
+  test = up
+
+
+def test_under():
+  global test
+  test = torre.negative()
+  under = dama.under(rey)
+  test = under
+
+
+def testHRepeat():
+  global test
+  test = torre.negative()
+  fila = peon.horizontalRepeat(8)
+  test = fila
+
+
+def testVRepeat():
+  global test
+  test = torre.negative()
+  columna = peon.verticalRepeat(8)
+  test = columna
+
+
+def test_rotateH():
+  global test
+  test = rey.rotate_horario()
+
+
+def test_rotateA():
+  global test
+  test = rey.rotate_antihorario()
+
+
+tests = [
+    test_espejos, test_invColor, test_join, test_up, test_under, testHRepeat,
+    testVRepeat, test_rotateH, test_rotateA
+]
+
+options = zip(tests, range(9))
+
+print(list(options))
+
+op = int(input("Elige el test a hacer: "))
+
+tests[op]()
+draw(test)
